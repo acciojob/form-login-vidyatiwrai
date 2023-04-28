1346ff0434ac6e5abe61aed0d7da3390e1b99551
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-28 07:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
-    //Write your code here
+function  getFormvalue(){
+        let fname = document.getElementById("fname");
+        let lname = document.getElementById("lname");
 
-}
+        fname = document.getElementById("fname").value;
+         lname = document.getElementById("lname").value;
+
+        let submit = document.getElementById("submit");
+
+        alert(fname+" "+lname);
+
+
+ }
+
+
+     submit.addEventListener("click",getFormvalue);
